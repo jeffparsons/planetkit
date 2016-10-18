2af0c942d2065b1c02dea03a458179b870844f96
Jeff Parsons <jeff@parsons.io>
2016-10-18 21:27:26 +1100

    Use `project` to build geometry

--- src/globe.rs
+++ src/globe.rs
@@ -30,6 +30,55 @@ impl Spec {
     }
 }
 
+// Project a position in a given root quad into a unit sphere.
+// Assumes that one corner is represented in `pt_in_root_quad`
+// as (0, 0) and the opposite is (1, 1).
+pub fn project(root: Root, pt_in_root_quad: Pt2) -> Pt3 {
+    // Each root quad comprises two triangles of the icosahedron.
+    // So we need to set up some points and vectors based on the
+    // root we're operating in, and then the math depends on which
+    // triangle `pt_in_root_quad` is in.
+    //
+    // See the diagram below for a visual description of how 2-space
+    // coordinates on the quad relate to the the icoshaedral vertices.
+    // `N` and `S` refer to the north and south triangles respectively.
+    //
+    //     a    ________  c (0, 1)
+    //  (0, 0)  \      /\    N_2
+    //    N_0    \ N  /  \   S_1
+    //            \  /  S \
+    //             \/______\
+    //                       d (1, 1)
+    //            b (1, 0)     S_0
+    //              N_1
+    //              S_2
+    //
+    // TODO: cache all this stuff somewhere. It's tiny, and we'll use it heaps.
+    use icosahedron::{ FACES, VERTICES };
+    let i_north = root.index as usize * 2;
+    let i_south = i_north + 1;
+    let north = FACES[i_north];
+    let south = FACES[i_south];
+    let a: Pt3 = (&VERTICES[north[0]]).into();
+    let b: Pt3 = (&VERTICES[north[1]]).into();
+    let c: Pt3 = (&VERTICES[north[2]]).into();
+    let d: Pt3 = (&VERTICES[south[0]]).into();
+
+    let ab = b - a;
+    let ac = c - a;
+    let db = b - d;
+    let dc = c - d;
+
+    // Decide which triangle we're in.
+    if pt_in_root_quad[0] + pt_in_root_quad[1] < 1.0 {
+        // In first triangle.
+        a + ab * pt_in_root_quad[0] + ac * pt_in_root_quad[1]
+    } else {
+        // In second triangle.
+        d + dc * (1.0 - pt_in_root_quad[0]) + db * (1.0 - pt_in_root_quad[1])
+    }
+}
+
 // TODO: split out a WorldGen type that handles all the procedural
 // generation, because none of that really needs to be tangled
 // with the realised Globe.
@@ -111,104 +160,53 @@ impl Globe {
 
     // Make vertices and list of indices into that array for triangle faces.
     pub fn make_geometry(&self) -> (Vec<::Vertex>, Vec<u16>) {
-        // Massage vertex data into form that gfx wants.
-        // Set up some noise so we can mutate the base
-        // icosahedron vertices.
-        let noise = noise::Brownian3::new(noise::perlin3, 4).wavelength(1.0);
-        let mutated_vertices: Vec<::Vertex> = icosahedron::VERTICES
-            .iter()
-            .map(|v| {
-                let (x, y, z) = (
-                    v[0] * self.spec.radius,
-                    v[1] * self.spec.radius,
-                    v[2] * self.spec.radius,
-                );
-                // Vary a little bit around 1.0.
-                let val = noise.apply(&self.pt, &[x, y, z]) * 0.1 + 1.0;
-                // Set the color later when we clone it.
-                // TODO: This code is a mess. Hack first, clean it up later.
-                ::Vertex::new([
-                    (x * val) as f32,
-                    (y * val) as f32,
-                    (z * val) as f32,
-                ], [0.0, 0.0, 0.0])
-            })
-            .collect();
-
-        // Give each face its own unique copies of the vertices,
-        // so that we can colour the faces independently.
+        // Output faces for each root quad.
         let mut vertex_data: Vec<::Vertex> = Vec::new();
-        let mut face_index: usize = 0;
-        let index_vec: Vec<u16> = icosahedron::FACES
-            .iter()
-            .flat_map(|f| {
-                let first_vertex_index = vertex_data.len();
-                for v in 0..3 {
-                    let mut colored_vertex = mutated_vertices[f[v]].clone();
-                    // Give adjacent triangles the same colour.
-                    // We want to highlight the connection between
-                    // the two triangles that make up each quad.
-                    colored_vertex.a_color = icosahedron::RAINBOW[face_index / 2];
-                    vertex_data.push(colored_vertex);
+        let index_vec: Vec<u16> = (0..ROOT_QUADS)
+            .flat_map(|root_index| {
+                let first_vertex_index = vertex_data.len() as u16;
+                // Add vertices of both north and south triangles of quad.
+                // Color by root quad.
+                let root_color = icosahedron::RAINBOW[root_index as usize];
+                let tri_coords: [[f64; 2]; 6] = [
+                    // TEMP: match the order of the icosahedron vertices
+                    // to minimise differences initially--but this shouldn't
+                    // need to be the same at all.
+                    // North triangle
+                    [0.0, 0.0],
+                    [1.0, 0.0],
+                    [0.0, 1.0],
+
+                    // South triangle
+                    [1.0, 1.0],
+                    [0.0, 1.0],
+                    [1.0, 0.0],
+                ];
+                for coord_pair in tri_coords.iter() {
+                    let pt3 = project(
+                        Root { index: root_index },
+                        Pt2::new(coord_pair[0], coord_pair[1]),
+                    );
+                    let vertex = ::Vertex::new([
+                        pt3[0] as f32,
+                        pt3[1] as f32,
+                        pt3[2] as f32,
+                    ], root_color);
+                    vertex_data.push(vertex);
                 }
-                face_index += 1;
+
                 vec![
+                    // North face
                     first_vertex_index,
                     first_vertex_index + 1,
                     first_vertex_index + 2,
+                    // South face
+                    first_vertex_index + 3,
+                    first_vertex_index + 4,
+                    first_vertex_index + 5,
                 ]
-            })
-            .map(|vi| vi as u16)
-            .collect();
+            }).collect();
 
         (vertex_data, index_vec)
-    }
-
-    // Project a position in a given root quad into a unit sphere.
-    // Assumes that one corner of `flat_point` is (0, 0) and the other is (1, 1).
-    pub fn project(root: Root, flat_point: Pt2) -> Pt3 {
-        // Each root quad comprises two triangles of the icosahedron.
-        // So we need to set up some points and vectors based on the
-        // root we're operating in, and then the math depends on which
-        // triangle `flat_point` is in.
-        //
-        // See the diagram below for a visual description of how 2-space
-        // coordinates on the quad relate to the the icoshaedral vertices.
-        // `N` and `S` refer to the north and south triangles respectively.
-        //
-        //     a    ________  c (0, 1)
-        //  (0, 0)  \      /\    N_2
-        //    N_0    \ N  /  \   S_1
-        //            \  /  S \
-        //             \/______\
-        //                       d (1, 1)
-        //            b (1, 0)     S_0
-        //              N_1
-        //              S_2
-        //
-        // TODO: cache all this stuff somewhere. It's tiny, and we'll use it heaps.
-        use icosahedron::{ FACES, VERTICES };
-        let i_north = root.index as usize * 2;
-        let i_south = i_north + 1;
-        let north = FACES[i_north];
-        let south = FACES[i_south];
-        let a: Pt3 = (&VERTICES[north[0]]).into();
-        let b: Pt3 = (&VERTICES[north[1]]).into();
-        let c: Pt3 = (&VERTICES[north[2]]).into();
-        let d: Pt3 = (&VERTICES[south[0]]).into();
-
-        let ab = b - a;
-        let ac = c - a;
-        let db = b - d;
-        let dc = d - c;
-
-        // Decide which triangle we're in.
-        if flat_point[0] + flat_point[1] < 1.0 {
-            // In first triangle.
-            a + ab * flat_point[0] + ac * flat_point[1]
-        } else {
-            // In second triangle.
-            d + dc * (1.0 - flat_point[0]) + db * (1.0 - flat_point[1])
-        }
     }
 }
