22c220cda9b8895a0cb2c03da6576e290ab261bc
Jeff Parsons <jeff@parsons.io>
2016-12-28 00:39:29 +1100

    Make dummy mesh a bit fancier
    It now has blobs for the whole rainbow!
    
    This is just temporary until I get around to building an axes mesh,
    with x=red, y=green, z=blue. That would be considerably more useful.

--- src/render/dummy_mesh.rs
+++ src/render/dummy_mesh.rs
@@ -3,7 +3,18 @@ use gfx;
 use super::{ System, MeshHandle, Vertex };
 use ::globe::icosahedron;
 
-const BRIGHT_RED: [f32; 3] = [ 1.0, 0.0, 0.0 ];
+pub const RAINBOW: [[f32; 3]; 10] = [
+    [ 1.0, 0.0, 0.0 ],
+    [ 1.0, 0.5, 0.0 ],
+    [ 1.0, 1.0, 0.0 ],
+    [ 0.5, 1.0, 0.0 ],
+    [ 0.0, 1.0, 0.0 ],
+    [ 0.0, 1.0, 0.5 ],
+    [ 0.0, 1.0, 1.0 ],
+    [ 0.0, 0.5, 1.0 ],
+    [ 0.0, 0.0, 1.0 ],
+    [ 0.5, 0.0, 1.0 ],
+];
 
 pub fn make_dummy_mesh<
     R: gfx::Resources,
@@ -13,23 +24,30 @@ pub fn make_dummy_mesh<
     factory: &mut F,
     render_system: &mut System<R, C>,
 ) -> MeshHandle {
-    let vertex_data: Vec<Vertex> = icosahedron::VERTICES
-        .iter()
-        .map(|v| {
-            Vertex::new([
-                // TODO: this multiplier is only because the globe I'm testing
-                // with is currently stupidly small.
-                v[0] as f32 * 0.01,
-                v[1] as f32 * 0.01,
-                v[2] as f32 * 0.01,
-            ], BRIGHT_RED)
-        })
-        .collect();
-    let index_vec: Vec<u32> = icosahedron::FACES
-        .iter()
-        .flat_map(|f| vec![f[0], f[1], f[2]])
-        .map(|vi| vi as u32)
-        .collect();
+    let mut vertex_data = Vec::<Vertex>::new();
+    let mut index_vec = Vec::<u32>::new();
+    let mut color_index = 0.0;
+    for color in RAINBOW.iter() {
+        let first_vertex_index = vertex_data.len() as u32;
+        for vertex in icosahedron::VERTICES.iter() {
+            vertex_data.push(
+                Vertex::new([
+                    // TODO: this multiplier is only because the globe I'm testing
+                    // with is currently stupidly small.
+                    vertex[0] as f32 * 0.01,
+                    vertex[1] as f32 * 0.01,
+                    // Space the blobs out a bit.
+                    vertex[2] as f32 * 0.01 + color_index * 0.03,
+                ], *color)
+            );
+        }
+        for face in icosahedron::FACES.iter() {
+            index_vec.push(first_vertex_index + face[0] as u32);
+            index_vec.push(first_vertex_index + face[1] as u32);
+            index_vec.push(first_vertex_index + face[2] as u32);
+        }
+        color_index += 1.0;
+    }
 
     // Register mesh with render system and return it.
     render_system.create_mesh(factory, vertex_data, index_vec)
