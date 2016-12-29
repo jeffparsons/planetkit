8331913076ee4f05aba626688175916028614832
Jeff Parsons <jeff@parsons.io>
2016-12-30 00:26:53 +1100

    Remove defunct dummy mesh

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -3,7 +3,6 @@ mod default_pipeline;
 mod mesh;
 mod encoder_channel;
 mod visual;
-mod dummy_mesh;
 mod axes_mesh;
 
 pub use self::system::{ System, MeshHandle };
@@ -11,5 +10,4 @@ pub use self::default_pipeline::Vertex;
 pub use self::mesh::Mesh;
 pub use self::encoder_channel::EncoderChannel;
 pub use self::visual::Visual;
-pub use self::dummy_mesh::make_dummy_mesh;
 pub use self::axes_mesh::make_axes_mesh;
