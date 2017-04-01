0ec572956f3ff0e0c611ef6ff4fadc5f17c9e2d0
Jeff Parsons <jeff@parsons.io>
2017-04-01 12:06:19 +0100

    Remove hodgepodge of nightly-related scripts
    They were all minimally useful. What I really needed was a super short
    bash function to have in my environment to run Cargo nightly with incremental
    compilation enabled.

--- util/bench.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Benching only works on unstable at the moment.
-cargo +nightly bench
--- util/lint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Clippy currently only works on nightly.
-# (This might eventually change if it gets folded into the official Rust distribution.)
-
-rustup run nightly cargo clippy "$@"
