d4a13b228b3f9f956b8976fc6a634a418e6eccb7
Jeff Parsons <jeff@parsons.io>
2017-10-04 20:08:10 +1100

    Move TCP stream handling into own function
    We're going to need this for the TCP client, too.

--- planetkit/src/net/tcp.rs
+++ planetkit/src/net/tcp.rs
@@ -8,7 +8,7 @@ use std::mem::size_of;
 use bytes::{BytesMut, BigEndian, ByteOrder};
 use futures::{self, Future};
 use tokio_core::reactor::Remote;
-use tokio_core::net::TcpListener;
+use tokio_core::net::{TcpListener, TcpStream};
 use tokio_io::codec::{Encoder, Decoder};
 use slog::Logger;
 use serde_json;
@@ -115,8 +115,6 @@ pub fn start_tcp_server<G: GameMessage, MaybePort>(
     let server_log = parent_log.new(o!());
     let server_error_log = server_log.new(o!());
 
-    let codec_log = parent_log.new(o!());
-
     remote.spawn(move |handle| {
         let socket = TcpListener::bind(&addr, &handle).expect("Failed to bind server socket");
         let actual_addr = socket.local_addr().expect("Socket isn't bound");
@@ -127,40 +125,12 @@ pub fn start_tcp_server<G: GameMessage, MaybePort>(
         actual_port_tx.send(actual_addr.port()).expect("Receiver hung up");
 
         let f = socket.incoming().for_each(move |(socket, peer_addr)| {
-            use tokio_io::AsyncRead;
-            let codec = Codec::<G>{
-                peer_addr: peer_addr,
-                log: codec_log.clone(),
-                _phantom_game_message: std::marker::PhantomData,
-            };
-            let stream = socket.framed(codec);
-            let peer_recv_system_sender = recv_system_sender.clone();
-            let peer_server_log = server_log.new(o!("peer_addr" => format!("{}", peer_addr)));
-            let peer_server_error_log = server_log.new(o!("peer_addr" => format!("{}", peer_addr)));
-            stream.filter(|recv_wire_message| {
-                // TODO: log
-                match recv_wire_message.message {
-                    Result::Err(_) => {
-                        println!("Got a bad message from peer");
-                        false
-                    }
-                    _ => true,
-                }
-            })
-            .for_each(move |recv_wire_message| {
-                // TODO: Only do this at debug level for a while, then demote to trace.
-                info!(peer_server_log, "Got recv_wire_message"; "recv_wire_message" => format!("{:?}", recv_wire_message));
-
-                // Send the message to net RecvSystem, to be interpreted and dispatched.
-                peer_recv_system_sender.send(recv_wire_message).expect("Receiver hung up?");
-
-                futures::future::ok(())
-            }).or_else(move |error| {
-                // Got a bad message from the peer (I assume) so the
-                // connection is going to close.
-                info!(peer_server_error_log, "Peer broke pipe"; "error" => format!("{}", error));
-                futures::future::ok(())
-            })
+            handle_tcp_stream(
+                socket,
+                peer_addr,
+                &server_log,
+                recv_system_sender.clone()
+            )
         }).or_else(move |error| {
             info!(server_error_log, "Something broke in listening for connections"; "error" => format!("{}", error));
             futures::future::ok(())
@@ -174,6 +144,53 @@ pub fn start_tcp_server<G: GameMessage, MaybePort>(
 
     // Wait until socket is bound before telling the caller what port we bound.
     actual_port_rx.recv().expect("Sender hung up")
+}
+
+// Handle sending/receiving and encoding/decoding messages
+// once a TCP stream (as either client or server) has been
+// established.
+fn handle_tcp_stream<G: GameMessage>(
+    socket: TcpStream,
+    peer_addr: SocketAddr,
+    parent_log: &Logger,
+    recv_system_sender: mpsc::Sender<RecvWireMessage<G>>,
+) -> Box<Future<Item=(), Error=std::io::Error>> {
+    use futures::Stream;
+    use tokio_io::AsyncRead;
+
+    let codec = Codec::<G>{
+        peer_addr: peer_addr,
+        log: parent_log.new(o!()),
+        _phantom_game_message: std::marker::PhantomData,
+    };
+    let stream = socket.framed(codec);
+    let peer_server_log = parent_log.new(o!("peer_addr" => format!("{}", peer_addr)));
+    let peer_server_error_log = peer_server_log.clone();
+    let f = stream.filter(|recv_wire_message| {
+        // TODO: log
+        match recv_wire_message.message {
+            Result::Err(_) => {
+                println!("Got a bad message from peer");
+                false
+            }
+            _ => true,
+        }
+    })
+    .for_each(move |recv_wire_message| {
+        // TODO: Only do this at debug level for a while, then demote to trace.
+        info!(peer_server_log, "Got recv_wire_message"; "recv_wire_message" => format!("{:?}", recv_wire_message));
+
+        // Send the message to net RecvSystem, to be interpreted and dispatched.
+        recv_system_sender.send(recv_wire_message).expect("Receiver hung up?");
+
+        futures::future::ok(())
+    }).or_else(move |error| {
+        // Got a bad message from the peer (I assume) so the
+        // connection is going to close.
+        info!(peer_server_error_log, "Peer broke pipe"; "error" => format!("{}", error));
+        futures::future::ok(())
+    });
+    Box::new(f)
 }
 
 #[cfg(test)]
