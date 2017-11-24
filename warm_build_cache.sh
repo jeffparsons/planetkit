98010f12622aaf296ac3c4d92fbe165defe54677
Jeff Parsons <jeff@parsons.io>
2017-11-24 18:51:03 +1100

    Add script to build everything
    Including tests and benches, in debug and release modes.
    Intended to make sure everything (including dependencies)
    is downloaded and built before hitting the road.

---
+++ warm_build_cache.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# Build everything in debug and release modes, including all tests
+# and benchmarks, to make sure we have all dependencies downloaded
+# and everything compiled.
+#
+# This is mostly useful if, e.g., you've just pulled down a new version
+# of the Rust compiler, and don't want surprise long compile wait times later.
+# (Especially useful if you're about to hit the road soon and don't
+# want to burn through your battery life and/or mobile data quota.)
+
+cargo build --tests --benches
+cargo build --tests --benches --release
